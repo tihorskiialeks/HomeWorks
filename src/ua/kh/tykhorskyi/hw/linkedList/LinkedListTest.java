3dd533ac43ca94166d1f576e76e8e803ec2bc748
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2022-09-06 08:10:49 +0300

    Update LinkedListTest.java

--- src/ua/kh/tykhorskyi/hw/linkedList/LinkedListTest.java
+++ src/ua/kh/tykhorskyi/hw/linkedList/LinkedListTest.java
@@ -3,7 +3,7 @@ package ua.kh.tykhorskyi.hw.linkedList;
 public class LinkedListTest {
 
 	public static void main(String[] args) {
-		List list1 = new List(12);
+		LinkedList list1 = new LinkedList(12);
 		list1.add(24);
 		list1.add(113);
 		list1.add(3000);
