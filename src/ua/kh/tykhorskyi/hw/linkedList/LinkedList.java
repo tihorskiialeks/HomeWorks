b0f8d9043ee49f512d71b13eea6745e9065d6e28
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2022-09-06 08:10:40 +0300

    Update LinkedList.java

--- src/ua/kh/tykhorskyi/hw/linkedList/LinkedList.java
+++ src/ua/kh/tykhorskyi/hw/linkedList/LinkedList.java
@@ -1,24 +1,54 @@
 package ua.kh.tykhorskyi.hw.linkedList;
 
+
 public class LinkedList {
-	private int number;
-	private LinkedList next;
+	private Item head;
+	private Item tail;
 
-	public int getNumber() {
-		return number;
+	public LinkedList(int data) {
+		this.head = new Item(data, null);
+		this.tail = head;
 	}
 
-	public LinkedList getNext() {
-		return next;
+	public void add(int data) {
+		Item temp = new Item(data, null);
+		tail.setNext(temp);
+		tail = tail.getNext();
 	}
 
-	public void setNext(LinkedList next) {
-		this.next = next;
+	public void add(int data, int index) {
+		Item list = head;
+		for (int i = 1; i < index - 1; i++) {
+			list = list.getNext();
+		}
+		list.setNext(new Item(data, list.getNext()));
 	}
 
-	public LinkedList(int number, LinkedList next) {
-		this.number = number;
-		this.next = next;
+	public void delete(int index) {
+		Item list = head;
+		for (int i = 1; i < index - 1; i++) {
+			list = list.getNext();
+		}
+		list.setNext(list.getNext().getNext());
 	}
 
+	public void print() {
+		Item list = head;
+		while (list != null) {
+			System.out.printf("%d ", list.getNumber());
+			list = list.getNext();
+		}
+	}
+
+	public void print(int index) {
+		Item list = head;
+		for (int i = 1; i < index; i++) {
+			list = list.getNext();
+		}
+		System.out.println(list.getNumber());
+	}
+
+	public void clean() {
+		head = new Item(0, null);
+	}
 }
