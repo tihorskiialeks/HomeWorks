c0e8dcf2c0870f9b84462ccb455bb1e53be4061a
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2022-09-16 11:31:43 +0300

    Update MathUtil.java

--- src/ua/kh/tykhorskyi/hw/mathUtil/MathUtil.java
+++ src/ua/kh/tykhorskyi/hw/mathUtil/MathUtil.java
@@ -161,18 +161,14 @@ public class MathUtil {
 	 */
 
 	public static void sortArray(int[] array) {
-		if (array.length != 0) {
-			for (int i = 0; i < array.length - 1; ++i) {
-				for (int j = 0; j < array.length - i - 1; ++j) {
-					if (array[j + 1] < array[j]) {
-						int swap = array[j];
-						array[j] = array[j + 1];
-						array[j + 1] = swap;
-					}
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					int swap = array[j];
+					array[j] = array[i];
+					array[i] = swap;
 				}
 			}
-		} else {
-			array = null;
 		}
 	}
 }
